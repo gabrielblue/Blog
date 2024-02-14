4a26a69f0564bede662958d8a1625927396a298a
JAMES KABOGO NJOROGE <kabogp@gmail.com>
2024-02-14 16:26:14 +0300

    my first commit

---
+++ src/components/BlogForm.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+const BlogForm = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const [body, setBody] = useState('');
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // onSave({ title, author, body });
+        setTitle('');
+        setAuthor('');
+        setBody('');
+    
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <h1 className="">Your blog</h1>
+            <div className="mb-3 p-2">
+                <label htmlFor="title" className="form-label">Title</label>
+                <input type="text" placeholder="Enter Title" className="form-control" id="title" value={title} onChange={(e) => setTitle(e.target.value)} />
+            </div>
+            <div className="mb-3 p-2">
+                <label htmlFor="author" className="form-label">Author</label>
+                <input type="text" className="form-control" id="author" placeholder="Enter Author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+            </div>
+            <div className="mb-3 p-2">
+                <label htmlFor="body" className="form-label">Body</label>
+                <textarea className="form-control" placeholder="Enter Body" id="body" value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                </div>
+            <button type="submit" className="btn btn-primary">Save Blog</button>
+            {/* <p>{title}</p>
+            <p>{author}</p>
+            <p>{body}</p> */}
+        </form>
+    )
+    
+}
+
+export default BlogForm;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+
+    return (
+    <nav className="navbar">
+            <h2>Blog Post</h2>
+            <div className="links">
+                <a href="Home">Home</a>
+                <a href="New Blog">New Blog</a>
+                </div>
+        </nav>
+    )
+}
+export default Navbar;
